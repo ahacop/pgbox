b0d9811eb62b986bb61ef3a5619ade946732f36a
Ara Hacopian <ara@hacopian.de>
2025-09-16 08:07:57 +0200

    lint: Fix flake formatting

--- flake.nix
+++ flake.nix
@@ -6,46 +6,49 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+      in {
         packages.default = pkgs.buildGoModule {
           pname = "pgbox";
           version = "0.1.0";
-          
+
           src = ./.;
-          
+
           vendorHash = "sha256-si74AW9Uu8l+zCG6PEcZVFZ/pQ8N4yGayYm82afwc5E=";
-          
+
           buildPhase = ''
             runHook preBuild
             go build -ldflags="-s -w" -o pgbox .
             runHook postBuild
           '';
-          
+
           installPhase = ''
             runHook preInstall
             mkdir -p $out/bin
             cp pgbox $out/bin/
             runHook postInstall
           '';
-          
+
           env.CGO_ENABLED = 0;
-          
+
           buildInputs = with pkgs; [
             docker
             docker-compose
             postgresql # For psql client at runtime
           ];
-          
+
           meta = with pkgs.lib; {
             description = "PostgreSQL-in-Docker development environment";
             homepage = "https://github.com/ahacop/pgbox";
             license = licenses.mit; # Update to match your license
-            maintainers = [ ];
+            maintainers = [];
           };
         };
 
@@ -58,5 +61,6 @@
             postgresql # For psql client
           ];
         };
-      });
+      }
+    );
 }
